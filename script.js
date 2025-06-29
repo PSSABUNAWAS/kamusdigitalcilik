40ac7ce14ada487236ba203361bfd6587d01db7a
PSSABUNAWAS <pssibnutufailskbt@gmail.com>
2025-06-29 21:16:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let data;
+
+fetch('data.json')
+  .then(response => response.json())
+  .then(json => data = json);
+
+function mulaSebut() {
+  const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+  recognition.lang = 'ms-MY';
+  recognition.start();
+
+  recognition.onresult = function(event) {
+    const said = event.results[0][0].transcript.toLowerCase();
+    alert("Anda sebut: " + said);  // Debug line to show what was heard
+
+    const result = data.find(item => item.perkataan.toLowerCase() === said);
+
+    if (result) {
+      document.getElementById('perkataan').innerText = result.perkataan;
+      document.getElementById('maksud').innerText = 'Maksud: ' + result.maksud;
+      document.getElementById('ayat').innerText = 'Contoh: ' + result.ayat;
+
+      const gabungan = new SpeechSynthesisUtterance("Maksud: " + result.maksud + ". Contoh: " + result.ayat);
+      gabungan.lang = 'ms-MY';
+      speechSynthesis.speak(gabungan);
+    } else {
+      document.getElementById('perkataan').innerText = said;
+      document.getElementById('maksud').innerText = 'Perkataan tidak dijumpai dalam senarai.';
+      document.getElementById('ayat').innerText = '';
+    }
+  };
+}
